3b40336752486d539062dcf551035caf81c0c281
Jangjingo457 <167618269+Lugi36123@users.noreply.github.com>
2024-09-24 20:22:05 +0900

    프로젝트 생성

---
+++ src/main/java/test02/drillsurvival/DrillSurvival.java
@@ -0,0 +1,17 @@
+package test02.drillsurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DrillSurvival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
